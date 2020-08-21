396583017b1de3bd77479431cc454c7aea88a7c0
USER <user@USERs-iMac-2.local>
2020-08-20 20:56:22 -0600

    fixed the assignment it is now completed

--- js/app.js
+++ js/app.js
@@ -1,31 +1,25 @@
 var age = [24, 36, 14, 18];
-var is_subscribed = ['yes', 'no', 'no', 'yes'];
+var is_subscribed = [true, false, false,true];
 
 arraylength= age.length;
-for(var counter=0; counter<arraylength; counter++){
-    console.log(age[counter]);
+for(var index=0; index<arraylength; index++){
+    console.log(age[index]);
+    if((age[index] < 18) && (is_subscribed[index] == false)){
+        console.log('The user is under 18 and not subscribed');
+    }
+    else if((age[index] >= 18) && (is_subscribed[index] == false)){
+        console.log('The user is 18 and older and not subscribed');
+    }
+    else if((age[index] < 18) && (is_subscribed[index] == true)){
+        console.log('The user is younger than 18 and is subscribed');
+    }
+    else if((age[index] >= 18) && (is_subscribed[index] == true)) {
+        console.log('The user is 18 and older and is subscribed');
+    }
+    
+    else{
+        console.log('error in the code')
+    }
+    
 }
 
-arraylength= is_subscribed.length
-for(var counter=0; counter<arraylength; counter++){
-    console.log(is_subscribed[counter]);
-}
-
-
-
-if((age < 18) && (is_subscribed == false)){
-    console.log('The user is under 18 and not subscribed');
-}
-else if((age >= 18) && (is_subscribed == false)){
-    console.log('The user is 18 and older and not subscribed');
-}
-else if((age < 18) && (is_subscribed == true)){
-    console.log('The user is younger than 18 and is subscribed');
-}
-else if((age >= 18) && (is_subscribed == true)) {
-    console.log('The user is 18 and older and is subscribed');
-}
-
-else{
-    console.log('error in the code')
-}
