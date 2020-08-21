ec5a9f5d8f560dc02045ec7eb710a841ee089ab8
USER <user@USERs-iMac-2.local>
2020-08-20 19:35:29 -0600

    created 2 arrays and 2 loops that spit out the array using console.log

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
-var age = 17;
-var is_subscribed = false;
+var age = [24, 36, 14, 18];
+var is_subscribed = ['yes', 'no', 'no', 'yes'];
+
+arraylength= age.length;
+for(var counter=0; counter<arraylength; counter++){
+    console.log(age[counter]);
+}
+
+arraylength= is_subscribed.length
+for(var counter=0; counter<arraylength; counter++){
+    console.log(is_subscribed[counter]);
+}
+
+
 
 if((age < 18) && (is_subscribed == false)){
     console.log('The user is under 18 and not subscribed');
@@ -17,4 +29,3 @@ else if((age >= 18) && (is_subscribed == true)) {
 else{
     console.log('error in the code')
 }
-    
